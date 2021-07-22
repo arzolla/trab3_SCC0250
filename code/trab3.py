e4e7a423529fc61808ac0a821d22a5b7ca50d131
Victor M. Arzolla <victor.arzolla@gmail.com>
2021-07-22 14:11:14 -0300

    Adicionado trab3.py

--- code/trab3.py
+++ code/trab3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #########################################
 ##### SCC0250 - Computação Gráfica ######
-####### Trabalho 2 - Cenário 3D #########
+####### Trabalho 3 - Iluminação #########
 #########################################
 #### Aluno: Victor de Mattos Arzolla ####
 #### NUSP: 9039312 ######################
